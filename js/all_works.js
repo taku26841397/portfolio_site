3580225da524ab08c517e2926475ae0cc83f1497
username <user@example>
2026-08-09 19:51:48 +0900

    Update

---
+++ js/all_works.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".filter-btn");
+const items = document.querySelectorAll(".project-item");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const filter = btn.dataset.filter;
+
+        // active クラス切り替え
+        buttons.forEach(b => b.classList.remove("now"));
+        btn.classList.add("now");
+
+        // 作品フィルタリング
+        items.forEach(item => {
+            if (filter === "all" || item.dataset.category === filter) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    });
+});
